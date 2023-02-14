97fb995ddc1417057a796c03cad7da6b8dda0897
Nickmescher <Ferbuscr22>
2023-02-15 02:27:24 +0300

    Lab-1 done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log(process.env.PORT)
+  const app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   if (typeof process.env.PORT === "undefined")
   {
     await app.listen(3000);
